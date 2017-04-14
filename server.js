d5494511ba2846760a51bba96ee2630c508e7c0f
cc <19948031@qq.com>
2017-04-14 11:18:24 +0800

    套用leancloud模板

--- server.js
+++ server.js
@@ -1,29 +1,16 @@
-'user strict'
-const fs = require('fs');
-const DOMParser = require('xmldom').DOMParser;
-fs.readFile('template.xml', 'utf-8', function (err, data) {
-    if (err) {
-        console.log(err);
-    } else {
-        // console.log(data);
-        let doc = new DOMParser().parseFromString(data, 'text/xml');
-        doc.getElementById('name').textContent = "cc";
-        doc.getElementById('name').textContent = "陈诚";
-        doc.getElementById('time').textContent = "2017-4-14";
-        doc.getElementById('toWho').textContent = "涂涂";
-        doc.getElementById('type').textContent = "服务";
-        let teststr = ``;
-        for (let i = 0; i < 300; i++) {
-            teststr += "测试";
-        }
-        doc.getElementById('content').textContent = teststr;
-        fs.writeFile('output.xml', doc, (err) => {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log('ok.');
-            }
-        })
-        // console.log(doc.getElementById('title').textContent);
-    }
+var AV = require('leanengine');
+
+AV.init({
+  appId: process.env.LEANCLOUD_APP_ID,
+  appKey: process.env.LEANCLOUD_APP_KEY,
+  masterKey: process.env.LEANCLOUD_APP_MASTER_KEY
+});
+
+var app = require('./app');
+
+// 端口一定要从环境变量 `LEANCLOUD_APP_PORT` 中获取。
+// LeanEngine 运行时会分配端口并赋值到该变量。
+var PORT = parseInt(process.env.LEANCLOUD_APP_PORT || 3000);
+var server = app.listen(PORT, function () {
+  console.log('Node app is running, port:', PORT);
 });
